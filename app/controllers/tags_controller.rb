9cd364f20bcc3acd2273426983ab8f73f76383ba
Yiling <yilingchen1@gmail.com>
2015-03-07 22:41:07 -0800

    finish tagging

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,13 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    Tag.delete(Tag.find(params[:id]))
+    redirect_to tags_path
+  end
 end
